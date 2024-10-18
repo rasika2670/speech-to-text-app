dfe107537df9166c8d0615f78f8071bff8718b37
rasika2670 <rasikamhaske26gmail.com>
2024-10-18 10:42:47 +0530

    commit

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -6,9 +6,16 @@ const FileUpload = () => {
     const [loading, setLoading] = useState(false);
     const [transcription, setTranscription] = useState('');
     const [downloadLink, setDownloadLink] = useState('');
+    const [fileName, setFileName] = useState("");
+    const [isFileSelected, setIsFileSelected] = useState(false);
 
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
+    const handleFileChange = (e) => {
+        if (e.target.files.length > 0) {
+            const selectedFile = e.target.files[0];
+            setFile(selectedFile);
+            setFileName(selectedFile.name);
+            setIsFileSelected(true);
+        }
     };
 
     const handleSubmit = async (event) => {
@@ -39,12 +46,24 @@ const FileUpload = () => {
     };
 
     return (
-        <div>
-            <h1>Speech to Text Converter</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="file" accept="audio/*, video/*" onChange={handleFileChange} required />
-                <button type="submit" disabled={loading}>Upload</button>
+        <div className='flex flex-col items-center p-4'>
+            <h1 className='font-extrabold text-4xl m-4'>Lecture Summarizer</h1>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-2 items-center'>
+            <div className="flex items-center justify-center w-full">
+                <label
+                    className={`flex flex-col items-center px-4 py-2 ${isFileSelected ? "bg-green-500 text-white" : "bg-white text-Blue"
+                        } rounded-lg shadow-lg tracking-wide uppercase border ${isFileSelected ? "border-green-500" : "border-Blue"
+                        } cursor-pointer hover:bg-Blue hover:text-white transition duration-100`}
+                >
+                    <span className="text-base leading-normal font-semibold">
+                        {fileName || "Choose a file"}
+                    </span>
+                    <input type="file" className="hidden" onChange={handleFileChange}/>
+                </label>
+            </div>
+                <button className='bg-[#489fb5] font-bold' type="submit" disabled={loading}>Upload</button>
             </form>
+
             {loading && <p>Loading... Please wait.</p>}
             {transcription && (
                 <div>
